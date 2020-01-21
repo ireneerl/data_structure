dd58ff249817a4de77c45d63ae32042348d6f31c
Irene Erlyn Wina Rachmawan <irene@Irenes-MacBook-Pro.local>
2020-01-21 16:13:55 +0900

    basic data structure

---
+++ linked_list.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct node{
+  int val;
+  node *next;
+}:
+
+int main(){
+  do{
+    int choice = 0 ;
+    cin >> choice;
+    switch (choice){
+      case 1:
+      std::cout << "inserting ->" << '\n';
+      cin >> x;
+      insert(x)
+    }
+
+  }while(choice != 0);
+  return 0;
+}
